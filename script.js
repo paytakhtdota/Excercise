3197b846d9594174103477f1b7a0f4d1442bb772
Mo Ansari <paytakhtdota@gmail.com>
2023-11-24 10:41:46 -0800

    Objects and Methods

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 // for finding a single number is a Prime number.
 function isPrime(num) {
     let counter = 0;
@@ -28,3 +29,22 @@ const primeFinder = (firstNum,secNum) => {
 }
 
 console.log(primeFinder(1,25));
+
+//Objects and Methods
+const MHA ={
+    firstName : "Mohammad",
+    middelName:"Hossain",
+    lastName:   "Ansari",
+    BoD:    1989,
+    favorites :["Movie","Coding","Family Matters","Video Games"],
+    age : function ageCal(){
+        const currentyear = new Date().getFullYear();
+        return currentyear-this.BoD;
+    }
+}
+
+
+console.log(MHA);
+console.log(MHA.age());
+
+
